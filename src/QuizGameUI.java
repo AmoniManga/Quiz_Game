29956600d5e2c71c6bba25424209ae186c04af5e
BSU Event Management System <mangaa0826@students.bowiestate.edu>
2026-05-10 12:46:43 -0400

    initial commit

---
+++ src/QuizGameUI.java
@@ -0,0 +1,7 @@
+import javax.swing.*; import java.util.*; import java.io.*;
+
+public class QuizGameUI {
+    private JPanel mainPanel;
+    private JButton button2Button;
+    private JButton button1Button;
+}
