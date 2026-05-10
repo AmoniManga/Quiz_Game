52efbc8625b5436ab685edd6f1ecb2cdf0e38770
BSU Event Management System <mangaa0826@students.bowiestate.edu>
2026-05-10 13:00:28 -0400

    initial commit 2

--- src/QuizGameUI.java
+++ src/QuizGameUI.java
@@ -4,4 +4,11 @@ public class QuizGameUI {
     private JPanel mainPanel;
     private JButton button2Button;
     private JButton button1Button;
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Quiz Game");
+        frame.setContentPane(new QuizGameUI().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
