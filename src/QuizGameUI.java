ce9c9842c2705d76034b54c7e6e153fd0daccfc3
BSU Event Management System <mangaa0826@students.bowiestate.edu>
2026-05-10 18:32:58 -0400

    initial commit 5

--- src/QuizGameUI.java
+++ src/QuizGameUI.java
@@ -7,48 +7,67 @@ public class QuizGameUI {
     private JButton btn1, btn2, btn3, btn4, btn5, btn6;
 
     public QuizGameUI() {
-        mainPanel = new JPanel(); headerLabel = new JLabel();
-        btn1 = new JButton(); btn2 = new JButton();
-        btn3 = new JButton(); btn4 = new JButton();
-        btn5 = new JButton(); btn6 = new JButton();
+        mainPanel = new JPanel();
+        headerLabel = new JLabel();
+        btn1 = new JButton();
+        btn2 = new JButton();
+        btn3 = new JButton();
+        btn4 = new JButton();
+        btn5 = new JButton();
+        btn6 = new JButton();
 
-        mainPanel.setLayout(null); mainPanel.setBackground(Color.WHITE);
+        mainPanel.setLayout(null);
+        mainPanel.setBackground(Color.WHITE);
+
         mainPanel.add(headerLabel);
-        mainPanel.add(btn1); mainPanel.add(btn2);
-        mainPanel.add(btn3); mainPanel.add(btn4);
-        mainPanel.add(btn5); mainPanel.add(btn6);
+        mainPanel.add(btn1);
+        mainPanel.add(btn2);
+        mainPanel.add(btn3);
+        mainPanel.add(btn4);
+        mainPanel.add(btn5);
+        mainPanel.add(btn6);
 
         headerLabel.setText("<html><center>Welcome to the CyberQuest quiz!<br>" +
                 "This assignment is for Dr. Agada's COSC 402 class, Software & OS Security.<br>" +
                 "Pick an option below to get started.</center></html>");
-        headerLabel.setBounds(10, 20, 480, 100);
+        headerLabel.setFont(new Font("Arial", Font.BOLD, 26));
+        headerLabel.setBounds(50, 40, 900, 160);
         headerLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        headerLabel.setForeground(Color.BLACK);
+
+        Font btnFont = new Font("Arial", Font.PLAIN, 20);
 
         btn1.setText("1. Login");
-        btn1.setBounds(100, 140, 300, 40);
+        btn1.setFont(btnFont);
+        btn1.setBounds(200, 220, 600, 60);
 
         btn2.setText("2. Password Security Challenge");
-        btn2.setBounds(100, 190, 300, 40);
+        btn2.setFont(btnFont);
+        btn2.setBounds(200, 300, 600, 60);
 
         btn3.setText("3. Phishing Detection Challenge");
-        btn3.setBounds(100, 240, 300, 40);
+        btn3.setFont(btnFont);
+        btn3.setBounds(200, 380, 600, 60);
 
         btn4.setText("4. Encryption Basics Challenge");
-        btn4.setBounds(100, 290, 300, 40);
+        btn4.setFont(btnFont);
+        btn4.setBounds(200, 460, 600, 60);
 
         btn5.setText("5. View Scores");
-        btn5.setBounds(100, 340, 300, 40);
+        btn5.setFont(btnFont);
+        btn5.setBounds(200, 540, 600, 60);
 
         btn6.setText("6. Exit");
-        btn6.setBounds(100, 390, 300, 40);
+        btn6.setFont(btnFont);
+        btn6.setBounds(200, 620, 600, 60);
     }
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("CyberQuest Quiz");
         QuizGameUI myUI = new QuizGameUI();
         frame.setContentPane(myUI.mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(520, 550);
+
+        frame.setSize(1000, 800);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     } {
